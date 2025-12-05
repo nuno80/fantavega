d7e03a7690de99fa29da514b7591534afc69b3f0
Nuno <nuno.80.al@gmail.com>
2025-12-05 22:59:46 +0100

    fix 6

--- src/components/league/LeagueSelector.tsx
+++ src/components/league/LeagueSelector.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 import { Check, ChevronDown, Shield } from "lucide-react";
